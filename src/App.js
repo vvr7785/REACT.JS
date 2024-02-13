c39e1073f867d5ac902d1b68d4a183e588661a1d
Vishnu Vardhan Reddy <gvvr2001@gmail.com>
2024-02-13 12:27:12 +0530

    employeereactapp

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Employees from './Employees'
 
 function App() {
   const data = [
-    {id:1001,name:'Rahul',workexp:'2 years',dob:new Date(2000,5,11)},
-    {id:1002,name:'Shiva',workexp:'5 years',dob:new Date(1996,4,16)},
-    {id:1003,name:'Sudheer',workexp:'10 years',dob:new Date(1987,10,7)}
+    {id:1001,name:'Rahul',workexp:2,dob:new Date(2000,5,11)},
+    {id:1002,name:'Shiva',workexp:5,dob:new Date(1996,4,16)},
+    {id:1003,name:'Sudheer',workexp:10,dob:new Date(1987,10,7)}
   ];
   return (
     <div className="App">
--- src/Employees.js
+++ src/Employees.js
@@ -2,9 +2,9 @@ import Employeesinfo from './Employeesinfo';
 function Employees(props){
     return (
         <div>
-             <Employeesinfo name={props.details[0].name} workexp={props.details[0].workexp} date={props.details[0].dob}/>
-            <Employeesinfo name={props.details[1].name} workexp={props.details[1].workexp} date={props.details[1].dob}/>
-            <Employeesinfo name={props.details[2].name} workexp={props.details[2].workexp} date={props.details[2].dob}/>
+             <Employeesinfo name={props.details[0].name} workexp={props.details[0].workexp+' years'} date={props.details[0].dob}/>
+            <Employeesinfo name={props.details[1].name} workexp={props.details[1].workexp+' years'} date={props.details[1].dob}/>
+            <Employeesinfo name={props.details[2].name} workexp={props.details[2].workexp+' years'} date={props.details[2].dob}/>
         </div>
     );
 }

e67d271771cdd468a166472ded19408489eb10bd
Vishnu Vardhan Reddy <gvvr2001@gmail.com>
2024-02-20 16:58:41 +0530

    EmployeeItem

--- src/Employees/EmployeeItem.js
+++ src/Employees/EmployeeItem.js
@@ -8,6 +8,14 @@ import DeleteData from './DeleteData';
 const EmployeeItem = (props) => {
     
     const [name, setName] = useState(props.name);
+
+    let we = "";
+
+    if(props.workexp>1){
+        we = <div className="expense-item_price">{props.workexp} years</div>
+    }else{
+        we = <div className="expense-item_price">{props.workexp} year</div>
+    }
     
 
     return (
@@ -16,8 +24,7 @@ const EmployeeItem = (props) => {
                 <EmployeeDate date={props.date} />
                 <div className="expense-item_description">
                     <h2>{name}</h2>
-                    <div className="expense-item_price">{props.workexp} years</div>
-                    
+                    {we}
                     <DeleteData employeeID = {props.employeeID}/>
                 </div>
             </Card>
